bca322cf045c49ec8adaa233a2b7aaed28a8b336
Lingfeng Fu <2871618714@qq.com>
2023-08-19 20:30:22 +0800

    属性图标高亮

--- Assets/Scripts/EventCard/IconicStats.cs
+++ Assets/Scripts/EventCard/IconicStats.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,11 +7,20 @@ public class IconicStats : MonoBehaviour
     [SerializeField] private Slider sliderIntelligence;
     [SerializeField] private Slider sliderVirtue;
     [SerializeField] private Slider sliderHealth;
+    [SerializeField] private Image intelligenceHighlight;
+    [SerializeField] private Image virtueHighlight;
+    [SerializeField] private Image healthHighlight;
 
     public float Intelligence
     {
         get => sliderIntelligence.value;
         set => sliderIntelligence.value = value;
+    }
+
+    public bool IntelligenceHighlighted
+    {
+        get => intelligenceHighlight.enabled;
+        set => intelligenceHighlight.enabled = value;
     }
 
     public float Virtue
@@ -19,9 +29,29 @@ public class IconicStats : MonoBehaviour
         set => sliderVirtue.value = value;
     }
 
+
+    public bool VirtueHighlighted
+    {
+        get => virtueHighlight.enabled;
+        set => virtueHighlight.enabled = value;
+    }
+
     public float Health
     {
         get => sliderHealth.value;
         set => sliderHealth.value = value;
+    }
+
+    public bool HealthHighlighted
+    {
+        get => healthHighlight.enabled;
+        set => healthHighlight.enabled = value;
+    }
+
+    public void ClearHighlights()
+    {
+        IntelligenceHighlighted = false;
+        VirtueHighlighted = false;
+        HealthHighlighted = false;
     }
 }
